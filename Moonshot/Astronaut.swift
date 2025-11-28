04e7e20d1613dbdd7e883afc6f730b8730187a81
Garrett Keyes <garrett.keyes@gmail.com>
2025-11-28 14:35:21 -0500

    Project setup, adding generic codable handling

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,8 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Garrett Keyes on 11/28/25.
+//
+
+import Foundation
---
+++ Moonshot/Mission.swift
@@ -0,0 +1,8 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Garrett Keyes on 11/28/25.
+//
+
+import Foundation
